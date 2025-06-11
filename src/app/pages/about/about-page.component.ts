683bd415731b9f453232778ec44f84997517b7f0
Juan Carlos Fernández García <jucarlos@jccm.es>
2025-06-11 13:29:47 +0200

    terminado

--- src/app/pages/about/about-page.component.ts
+++ src/app/pages/about/about-page.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+// import { isPlatformBrowser } from '@angular/common'
 
 @Component({
   selector: 'app-about-page',
@@ -7,4 +10,42 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./about-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent { }
+export default class AboutPageComponent implements OnInit {
+
+
+
+  private title = inject( Title );
+  private meta = inject( Meta );
+
+  //private platform = inject( PLATFORM_ID );
+
+  ngOnInit(): void {
+   
+    console.log('Mi primera app');
+
+    // if ( isPlatformBrowser ( this.platform )) {
+    //   console.log('Estoy en el browser');
+    // } else {
+    //   console.log('Estoy en el server');
+    // }
+
+    this.title.setTitle('Pagina about');
+
+    this.meta.updateTag(
+      { name: 'description', content: 'Esta es una aplicación muy chula'}
+    );
+
+    this.meta.updateTag(
+      { name: 'og:title', content: 'Esta etiqueta es apra instagram'}
+    );
+
+    this.meta.updateTag(
+      { name: 'og:keywords', content: 'curso, angular, carlos, toledo'}
+    );
+
+
+
+  } 
+
+
+}
